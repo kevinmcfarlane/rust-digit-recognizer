0d3fbd52188836786386aaeae0ae751628396bcc
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2021-04-15 15:52:17 +0100

    Implement Evaluator (compiles).

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,11 @@ pub struct Observation {
 }
 
 impl Observation {
-    pub fn new(label: String, pixels: Vec<i32>) -> Observation {
+    pub fn new(label: &str, pixels: &Vec<i32>) -> Observation {
         let label = label;
         let pixels = pixels;
 
-        Observation { label, pixels }
+        Observation { label: label.to_string(), pixels: pixels.to_vec() }
     }
 }
 
@@ -46,15 +46,15 @@ impl ManhattanDistance {
 }
 
 pub struct BasicClassifier {
-    data: Vec<Observation>
+    //data: Vec<Observation>
 }
 
 impl BasicClassifier {
-    pub fn new(data: Vec<Observation>) -> BasicClassifier {
-        let data = data;
+    // pub fn new(data: Vec<Observation>) -> BasicClassifier {
+    //     let data = data;
 
-        BasicClassifier { data }
-    }
+    //     BasicClassifier { data }
+    // }
 
     /// Predicts the digit that the image corresponds to.
     ///
@@ -62,16 +62,18 @@ impl BasicClassifier {
     ///
     /// `pixels` -  The pixels representing the image.
     ///
-    pub fn predict(self, pixels: &Vec<i32>) -> String {
+    pub fn predict(data: &Vec<Observation>, pixels: &Vec<i32>) -> String {
         let mut shortest = f64::MAX;
-        let mut current_best = Observation::new(String::from(""), vec![0]);
-        let data = self.data;
+        
+        let default_label = "";
+        let default_pixels: Vec<i32> = Vec::new();
+        let mut current_best = Observation::new(default_label, &default_pixels);
 
         for obs in data {
             let dist = ManhattanDistance::between(&obs.pixels, &pixels);
             if dist < shortest {
                 shortest = dist;
-                current_best = obs;
+                current_best = Observation::new(&obs.label, &obs.pixels);
             }
         }
 
@@ -79,21 +81,44 @@ impl BasicClassifier {
     }
 }
 
-pub struct Evaluator {}
+pub struct Evaluator {
+    training_set: Vec<Observation>,
+}
 
 impl Evaluator {
+    pub fn new(training_set: Vec<Observation>) -> Evaluator {
+        let training_set = training_set;
+
+        Evaluator { training_set }
+    }
+
     /// "Scores" the prediction by comparing what the classifier predicts with the true value. If they match,
     /// we record a 1, otherwise we record a 0. By using numbers like this rather than true/false values, we can
     /// average this out to get the percentage correct.
-    pub fn score(obs: &Observation, classifier: &BasicClassifier) -> f64 {
+    pub fn score(&self, obs: Observation) -> f64 {
         let label = obs.label;
-        let prediction = &classifier.predict(&obs.pixels);
+        let prediction = BasicClassifier::predict(&self.training_set, &obs.pixels);
         
         if label == prediction {
             1.0
         } else {
             0.0
         }
+    }
+
+    pub fn correct(&self, validation_set: Vec<Observation>) -> f64 {
+        let mut scores: Vec<f64> = Vec::new();
+        let number_of_scores = validation_set.len();
+
+        for obs in validation_set {
+            let score = *(&self.score(obs));
+            scores.push(score);
+        }
+
+        let sum: f64 = Iterator::sum(scores.iter());
+        let average = f64::from(sum) / (number_of_scores as f64);
+
+        average
     }
 }
 
@@ -122,7 +147,7 @@ fn main()
             pixels.push(pixel);
         }
 
-        let observation = Observation::new(String::from(label), pixels);
+        let observation = Observation::new(label, &pixels);
 
         observations.push(observation);
     }
